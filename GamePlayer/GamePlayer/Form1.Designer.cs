e6c1f229e4a6f53097987ec66e5e830e64f4bc88
d.efimov <d.efimov@simcase.ru>
2015-01-24 19:32:17 +0300

    Added drawing objects and generated barriers.

--- GamePlayer/GamePlayer/Form1.Designer.cs
+++ GamePlayer/GamePlayer/Form1.Designer.cs
@@ -49,6 +49,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(400, 400);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // label1
             // 
@@ -108,6 +109,7 @@
             // 
             this.timer1.Enabled = true;
             this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // openFileDialog1
             // 
