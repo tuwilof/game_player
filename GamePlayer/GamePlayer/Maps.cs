4c9cbfff6245ab6e36a859ee2df73b711ac773ea
d.efimov <d.efimov@simcase.ru>
2015-01-28 03:20:59 +0300

    Now maps levels load in file.

---
+++ GamePlayer/GamePlayer/Maps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePlayer
+{
+
+    public class Maps
+    {
+        public Constructorm Constructorm { get; set; }
+        public List<Mainm> Mainm { get; set; }
+    }
+
+    public class Constructorm
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class Mainm
+    {
+        public List<Levelm> Levelm { get; set; }
+    }
+
+    public class Levelm
+    {
+        public Positionm Positionm { get; set; }
+    }
+
+    public class Positionm
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
+
